fb1486436f46ff81fa803dc02f05dc8303966d00
Matt Green <matthewngreen@speedpost.net>
2022-07-01 21:50:28 +0100

    Added MIT copyright notices

--- calendar.h
+++ calendar.h
@@ -1,10 +1,24 @@
+/* ------------------------------------------------------------------------------ 
+MIT License
 
+Copyright (c) 2022 Matthew Nathan Green
 
-// This class represents a generalized calendar. The Gregorian specific implementation
-// will be injected as a strategy.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+-------------------------------------------------------------------------------- */
 
 #ifndef CALENDAR_H
 #define CALENDAR_H
+
+// This class represents a generalized calendar. The Gregorian specific implementation
+// will be injected as a strategy.
 
 #include <iostream>
 #include <vector>
--- gregorian_impl.h
+++ gregorian_impl.h
@@ -1,3 +1,21 @@
+/* ------------------------------------------------------------------------------ 
+MIT License
+
+Copyright (c) 2022 Matthew Nathan Green
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+-------------------------------------------------------------------------------- */
+
+#ifndef GREGORIAN_IMPL_H
+#define GREGORIAN_IMPL_H
 
 // This class is a stateless implementation of the worker functions required by the calendar.
 
@@ -6,9 +24,6 @@
 
 // This class ITSELF must leverage a month class, but THIS class should be testable independently
 // of that, so the month class itself is parametric.
-
-#ifndef GREGORIAN_IMPL_H
-#define GREGORIAN_IMPL_H
 
 #include <iostream>
 #include <vector>
