824876da98d43ba71eb7183c829c8a099f4d4b5d
Matt Green <matthewngreen@speedpost.net>
2024-11-16 13:33:06 +0000

    Altered auto generated years to include 1582 and 2024 Updated gitignore to ignore .cache

--- examples.sh
+++ examples.sh
@@ -5,7 +5,7 @@ echo Target folder set to : $TARGET_FOLDER
 cmake -Bbuild -GNinja .
 cmake --build build
 
-for i in 1800 1900 2000 2017
+for i in 1582 1800 1900 2024
 do
 ./build/calendar -y $i -o $TARGET_FOLDER/example_$i
 done
